e38b1902094342f472d6c8cb82ac56c658c688fb
Iván Luna <133716241+imprvhub@users.noreply.github.com>
2025-08-01 23:34:02 -0300

    feat(auth): integrate user authentication and db
    Add a comprehensive user authentication system, including Google OAuth, account management, and user profile handling. This new feature is supported by integrating Drizzle ORM for database persistence and schema management.

---
+++ src/content.config.ts
@@ -0,0 +1,8 @@
+// https://docs.astro.build/en/guides/content-collections/#defining-collections
+
+import { z, defineCollection } from 'astro:content';
+import { docsSchema } from '@astrojs/starlight/schema';
+
+export const collections = {
+  docs: defineCollection({ schema: docsSchema() }),
+};
